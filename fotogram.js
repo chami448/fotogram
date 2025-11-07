8e578418346fcd90af91f2e87ce5dfe71ac2040e
chami448 <155586123+chami448@users.noreply.github.com>
2025-11-07 07:59:40 +0100

    new JS code

---
+++ fotogram.js
@@ -0,0 +1,14 @@
+const animalImages = [
+    "birds.jpg", 
+    "elephants_1.jpg",
+    "elephants_2.jpg",
+    "gazelle.jpg",
+    "hippo.jpg",
+    "leopard_1.jpg",
+    "leopard_2.jpg",
+    "lion_1.jpg",
+    "lion_2.jpg",
+    "vulture.jpg"
+];
+
+
