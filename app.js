94ebc974983a1d273a2a569dc163d4c96224b7fd
chami448 <155586123+chami448@users.noreply.github.com>
2025-12-27 19:57:46 +0100

    cleanup with prettier

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-const main = document.getElementById('main');
-const dialog = document.getElementById('dialog');
+const main = document.getElementById("main");
+const dialog = document.getElementById("dialog");
 
 let dialogTitle;
 let dialogImage;
@@ -10,99 +9,102 @@ let closeBtn;
 let prevBtn;
 let nextBtn;
 
-
 const ANIMALS_DATA = {
-    animals: [
-        {
-            id: 1,
-            title: "Flamingos",
-            file: "./img/birds.jpg",
-            alt: "birds",
-            description: "Elegante rosa Flamingos in ihrer natürlichen Umgebung"
-        },
-        {
-            id: 2,
-            title: "Elefanten",
-            file: "./img/elephants_1.jpg",
-            alt: "Elefanten",
-            description: "Majestätische Elefanten in der Wildnis"
-        },
-        {
-            id: 3,
-            title: "Elefanten",
-            file: "./img/elephants_2.jpg",
-            alt: "Elefanten",
-            description: "Elefanten beim Trinken am Wasserloch"
-        },
-        {
-            id: 4,
-            title: "Gazelle",
-            file: "./img/gazelle.jpg",
-            alt: "Gazelle",
-            description: "Anmutige Gazelle in der Savanne"
-        },
-        {
-            id: 5,
-            title: "Nilpferd",
-            file: "./img/hippo.jpg",
-            alt: "Nilpferd",
-            description: "Nilpferd entspannt am Strand"
-        },
-        {
-            id: 6,
-            title: "Leopard",
-            file: "./img/leopard_1.jpg",
-            alt: "Leopard",
-            description: "Getarnter Leopard auf der Jagd im hohen Gras"
-        },
-        {
-            id: 7,
-            title: "Leopard",
-            file: "./img/leopard_2.jpg",
-            alt: "Leopard",
-            description: "Ruhender Leopard entspannt auf einem Baumast"
-        },
-        {
-            id: 8,
-            title: "Löwe",
-            file: "./img/lion_1.jpg",
-            alt: "Löwe",
-            description: "Stolzer Löwe mit prächtiger Mähne als König der Tiere"
-        },
-        {
-            id: 9,
-            title: "Löwe",
-            file: "./img/lion_2.jpg",
-            alt: "Löwe",
-            description: "Löwe ruhend in der warmen Savanne"
-        },
-        {
-            id: 10,
-            title: "Geier",
-            file: "./img/vulture.jpg",
-            alt: "Geier",
-            description: "Geier auf einem Baum "
-        }
-    ]
+  animals: [
+    {
+      id: 1,
+      title: "Flamingos",
+      file: "./img/birds.jpg",
+      alt: "birds",
+      description: "Elegante rosa Flamingos in ihrer natürlichen Umgebung",
+    },
+    {
+      id: 2,
+      title: "Elefanten",
+      file: "./img/elephants_1.jpg",
+      alt: "Elefanten",
+      description: "Majestätische Elefanten in der Wildnis",
+    },
+    {
+      id: 3,
+      title: "Elefanten",
+      file: "./img/elephants_2.jpg",
+      alt: "Elefanten",
+      description: "Elefanten beim Trinken am Wasserloch",
+    },
+    {
+      id: 4,
+      title: "Gazelle",
+      file: "./img/gazelle.jpg",
+      alt: "Gazelle",
+      description: "Anmutige Gazelle in der Savanne",
+    },
+    {
+      id: 5,
+      title: "Nilpferd",
+      file: "./img/hippo.jpg",
+      alt: "Nilpferd",
+      description: "Nilpferd entspannt am Strand",
+    },
+    {
+      id: 6,
+      title: "Leopard",
+      file: "./img/leopard_1.jpg",
+      alt: "Leopard",
+      description: "Getarnter Leopard auf der Jagd im hohen Gras",
+    },
+    {
+      id: 7,
+      title: "Leopard",
+      file: "./img/leopard_2.jpg",
+      alt: "Leopard",
+      description: "Ruhender Leopard entspannt auf einem Baumast",
+    },
+    {
+      id: 8,
+      title: "Löwe",
+      file: "./img/lion_1.jpg",
+      alt: "Löwe",
+      description: "Stolzer Löwe mit prächtiger Mähne als König der Tiere",
+    },
+    {
+      id: 9,
+      title: "Löwe",
+      file: "./img/lion_2.jpg",
+      alt: "Löwe",
+      description: "Löwe ruhend in der warmen Savanne",
+    },
+    {
+      id: 10,
+      title: "Geier",
+      file: "./img/vulture.jpg",
+      alt: "Geier",
+      description: "Geier auf einem Baum ",
+    },
+  ],
 };
 
-function MainGalleryTemplate(){
-    return `
+function MainGalleryTemplate() {
+  return `
         <section class="main-content">
-            ${ANIMALS_DATA.animals.map((animal, id) => `
+            ${ANIMALS_DATA.animals
+              .map(
+                (animal, id) => `
                 <div class="cards-container" onclick="openDialog(${id})" tabindex="0">
                     <figure>
                         <img src="${animal.file}" alt="${animal.alt}">
                         <figcaption>${animal.title}</figcaption>
                     </figure>
                 </div>
-            `).join('')}
+            `
+              )
+              .join("")}
         </section>
     `;
-};
+}
 
 function createDialogTemplate() {
-    return `
+  return `
         <section class="dialogContainer" aria-label="Safari Tiergalerie" tabindex="0">
             <div class="dialog-header">
                 <h2 id="dialogTitle"></h2>
@@ -123,108 +125,107 @@ function createDialogTemplate() {
             </div>
         </section>
     `;
-};
+}
 
-function setupDOM(){
-    main.innerHTML = MainGalleryTemplate();
-    dialog.innerHTML = createDialogTemplate();
-};
+function setupDOM() {
+  main.innerHTML = MainGalleryTemplate();
+  dialog.innerHTML = createDialogTemplate();
+}
 
-function dialogElements(){
-    dialogTitle = document.getElementById('dialogTitle');
-    dialogImage = document.getElementById('dialogImage');
-    dialogDescription = document.getElementById('dialogDescription');
-    pictureCounter = document.getElementById('pictureCounter');
-    closeBtn = document.getElementById('closeBtn');
-    prevBtn = document.getElementById('prevBtn');
-    nextBtn = document.getElementById('nextBtn');
-};
+function dialogElements() {
+  dialogTitle = document.getElementById("dialogTitle");
+  dialogImage = document.getElementById("dialogImage");
+  dialogDescription = document.getElementById("dialogDescription");
+  pictureCounter = document.getElementById("pictureCounter");
+  closeBtn = document.getElementById("closeBtn");
+  prevBtn = document.getElementById("prevBtn");
+  nextBtn = document.getElementById("nextBtn");
+}
 
-function updateDialogContent(id){
-    const animal = ANIMALS_DATA.animals[id];
-    dialogTitle.textContent = animal.title;
-    dialogImage.src = animal.file;
-    dialogImage.alt = animal.alt;
-    dialogDescription.textContent = animal.description;
-    pictureCounter.textContent = `${id + 1} / ${ANIMALS_DATA.animals.length}`;
-    prevBtn.onclick = function(){
-        let buttonIndex;
-        if(id === 0){
-            buttonIndex = ANIMALS_DATA.animals.length - 1;
-        } else {
-            buttonIndex = id - 1;
-        }
-        updateDialogContent(buttonIndex);
+function updateDialogContent(id) {
+  const animal = ANIMALS_DATA.animals[id];
+  dialogTitle.textContent = animal.title;
+  dialogImage.src = animal.file;
+  dialogImage.alt = animal.alt;
+  dialogDescription.textContent = animal.description;
+  pictureCounter.textContent = `${id + 1} / ${ANIMALS_DATA.animals.length}`;
+  prevBtn.onclick = function () {
+    let buttonIndex;
+    if (id === 0) {
+      buttonIndex = ANIMALS_DATA.animals.length - 1;
+    } else {
+      buttonIndex = id - 1;
     }
-    nextBtn.onclick = function(){
-        let buttonIndex;
-        if ( id === ANIMALS_DATA.animals.length - 1){
-            buttonIndex = 0;
-        } else{
-            buttonIndex = id + 1;
-        }
-        updateDialogContent(buttonIndex);
+    updateDialogContent(buttonIndex);
+  };
+  nextBtn.onclick = function () {
+    let buttonIndex;
+    if (id === ANIMALS_DATA.animals.length - 1) {
+      buttonIndex = 0;
+    } else {
+      buttonIndex = id + 1;
     }
+    updateDialogContent(buttonIndex);
+  };
+}
 
-};
+function openDialog(id) {
+  updateDialogContent(id);
+  dialog.showModal();
+}
 
-function openDialog(id){
-    updateDialogContent(id);
-    dialog.showModal();
-};
+function closeDialog() {
+  dialog.close();
+}
 
-function closeDialog(){
-    dialog.close();
-};
+function keyboardEvent() {
+  const cards = document.querySelectorAll(".cards-container");
 
-function keyboardEvent(){
-    const cards = document.querySelectorAll('.cards-container');
-
-    cards.forEach((card, id) =>{
-        card.addEventListener('keydown', function(e){
-            if (e.key === 'Enter' || e.key === ' '){
-                e.preventDefault();
-                openDialog(id);
-            }
-        })
-    })
-};
-
-function escapeKey(){
-    document.addEventListener('keydown', function(e){
-        if (e.key === 'Escape' && dialog && dialog.open) { 
-            closeDialog();
-        }
-    })
-};
-
-function arrowDialogKeys(){
-    document.addEventListener('keydown', function(e){
-        if (dialog && dialog.open){
-            if (e.key === 'ArrowLeft'){
-                document.getElementById('prevBtn').click();
-            } else if (e.key === 'ArrowRight'){
-                document.getElementById('nextBtn').click();
-            }
-        }
-    })
-};
-
-function outsideClickClose(){
-    dialog.addEventListener('click', function(e) {
-        if (e.target === dialog) {
-            closeDialog();
-        }
+  cards.forEach((card, id) => {
+    card.addEventListener("keydown", function (e) {
+      if (e.key === "Enter" || e.key === " ") {
+        e.preventDefault();
+        openDialog(id);
+      }
     });
-};
+  });
+}
 
-function init(){
-    setupDOM();
-    dialogElements();
-    keyboardEvent();
-    escapeKey();
-    arrowDialogKeys();
-    outsideClickClose();
+function escapeKey() {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && dialog && dialog.open) {
+      closeDialog();
+    }
+  });
+}
+
+function arrowDialogKeys() {
+  document.addEventListener("keydown", function (e) {
+    if (dialog && dialog.open) {
+      if (e.key === "ArrowLeft") {
+        document.getElementById("prevBtn").click();
+      } else if (e.key === "ArrowRight") {
+        document.getElementById("nextBtn").click();
+      }
+    }
+  });
+}
+
+function outsideClickClose() {
+  dialog.addEventListener("click", function (e) {
+    if (e.target === dialog) {
+      closeDialog();
+    }
+  });
+}
+
+function init() {
+  setupDOM();
+  dialogElements();
+  keyboardEvent();
+  escapeKey();
+  arrowDialogKeys();
+  outsideClickClose();
 }
 
 window.onload = init;
